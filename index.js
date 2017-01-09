d413314c5cce385b69e42fa1661696d8844d395b
Muneer PP <abdulmuneer22@gmail.com>
2017-01-09 21:19:08 +0530

    body parser

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
 var axios = require('axios')
+var bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser());
 
 
 var server = app.listen(3900, function () {
@@ -13,4 +18,13 @@ var server = app.listen(3900, function () {
 
 app.get('/',(req,res)=>{
     res.send("Hello World !!!")
+})
+
+app.post('/login',(req,res)=>{
+
+// var username = req.body.username
+// var password = req.body.password
+var username = req.body.username
+
+res.send("Hello" + username)
 })
