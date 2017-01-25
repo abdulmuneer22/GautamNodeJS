9a32d2cad724e3567d1b1d5c190f4f3c149b02b3
Muneer PP <abdulmuneer22@gmail.com>
2017-01-25 21:42:19 +0530

    added all CURD Method for mongodb

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser());
 
+assert = require('assert');
+var MongoClient = require('mongodb').MongoClient
+var url = 'mongodb://admin:admin@ds127389.mlab.com:27389/fatwallet_db';
 
 var server = app.listen(3900, function () {
    var host = server.address().address
@@ -47,4 +50,87 @@ app.get('/',(req,res)=>{
 
 
 })
+
+
+
+app.get('/mongotest/insert',(req,res)=>{
+    MongoClient.connect(url, function(err, db) {
+        assert.equal(null, err);
+        console.log("Connected correctly to server");
+
+        var item = {
+            "name" : "from nodejs"
+        }
+
+        db.collection('students').insertOne(item,function(err,db){
+        assert.equal(null, err);
+        console.log("Inserted correctly !!")
+        res.send("Update Complete !!")
+        db.close()
+        })
+
+    })
+})
+
+
+app.get('/mongotest/update',(req,res)=>{
+
+
+
+    MongoClient.connect(url, function(err, db) {
+        assert.equal(null, err);
+        console.log("Connected correctly to server");
+
+        
+
+        db.collection('students').update({"name" : "James!!!!"}, {"name" : "James"} )
+        db.close()
+        res.send("Updated Succefully !!")
+    
+    })
+})
+
+
+
+app.get('/mongotest/delete',(req,res)=>{
+
+
+
+    MongoClient.connect(url, function(err, db) {
+        assert.equal(null, err);
+        console.log("Connected correctly to server");
+
+        
+
+        db.collection('students').deleteOne({"name" : "James"})
+        db.close()
+        res.send("Deleted Succefully !!")
+    
+    })
+})
+
+
+app.get('/mongotest/read',(req,res)=>{
+    MongoClient.connect(url, function(err, db) {
+        assert.equal(null, err);
+        console.log("Connected correctly to server");
+
+        db.collection('students',(err,collection)=>{
+            if(!err){
+                collection.find({}).toArray((err,docs)=>{
+                    if(err){
+                        db.close()
+                        
+                    }
+
+                    res.send(docs)
+                    db.close()
+                })
+            }
+        })
+        
+
+    })
+})
+
 
